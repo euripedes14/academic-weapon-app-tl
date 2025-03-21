3dc12a8ebdb3ae597106b8c921bf2626709f9c7e
billystein25 <59233918+billystein25@users.noreply.github.com>
2025-03-21 19:18:06 +0000

    Updated homescreen.py to call statistics screen

--- homescreen.py
+++ homescreen.py
@@ -3,6 +3,7 @@ from tkinter import messagebox
 from schedule_screen import open_schedule     # Import the open_schedule function
 from mam import open_nutrition                # Import the open_nutrition function
 from settings import SettingsMenuApp
+from statistics_screen import StatisticsClass
 
 
 def open_settings(parent_frame):
@@ -14,6 +15,16 @@ def open_settings(parent_frame):
     settings_app = SettingsMenuApp(parent_frame)  # Pass the parent frame as the root
     settings_app.main_frame.pack(fill="both", expand=True)
 
+def open_statistics(parent_frame):
+    ## Display the statistics screen
+    for widget in parent_frame.winfo_children():  # Clear previous contents of the frame
+        widget.destroy()
+
+    # Create and display the StatisticsApp in the parent frame
+    statistics_app = StatisticsClass(parent_frame)  # Pass the parent frame as the root
+    statistics_app.main_frame.pack(fill="both", expand=True)
+    
+
 class HomeScreen:
     def __init__(self, root):
         self.root = root
@@ -24,6 +35,7 @@ class HomeScreen:
         self.schedule_frame = tk.Frame(root, bg="#f2f2f2")
         self.nutrition_frame = tk.Frame(root, bg="#f2f2f2")
         self.settings_frame = None
+        self.statistics_frame = None
 
         self.create_menu()
         self.create_title()
@@ -36,6 +48,7 @@ class HomeScreen:
         menu_bar.add_command(label="Τι έχει το πρόγραμμα?", command=self.show_schedule)
         menu_bar.add_command(label="Τι θα φάμε σήμερα?", command=self.show_nutrition)
         menu_bar.add_command(label="Ειδοποιήσεις & Streaks", command=self.open_notifications)
+        menu_bar.add_command(label="Στατιστικά", command=self.show_statistics)
         menu_bar.add_command(label="Ρυθμίσεις", command=self.show_settings)  # Σύνδεση με τη ρύθμιση
 
     def create_title(self):
@@ -54,6 +67,8 @@ class HomeScreen:
         self.nutrition_frame.pack_forget()
         if self.settings_frame:
             self.settings_frame.pack_forget()
+        if self.statistics_frame:
+            self.statistics_frame.pack_forget()
 
     def show_nutrition(self):
         """Δείξε το παράθυρο διατροφής."""
@@ -79,6 +94,19 @@ class HomeScreen:
         # Use the new function to display settings
         open_settings(self.settings_frame)
         self.settings_frame.pack(fill=tk.BOTH, expand=True)
+    
+    def show_statistics(self):
+        ## Display the statistics screen
+
+        self.hide_all_frames()
+        if not self.statistics_frame:
+            self.statistics_frame = tk.Frame(self.root, bg='#ffffff')
+            self.statistics_frame.pack(fill=tk.BOTH, expand=True)
+
+        open_statistics(self.statistics_frame)
+        self.statistics_frame.pack(fill=tk.BOTH, expand=True)
+
+
 
 if __name__ == "__main__":
     root = tk.Tk()
